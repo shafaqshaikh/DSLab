5b7d5fa9b600183583e20fca5f8fc3f043b56d28
shafaqshaikh <shafaqshaikh10.gmail.com>
2017-09-21 12:33:11 +0530

    binarysearchtree.c

--- binarysearchtree.c
+++ binarysearchtree.c
@@ -19,14 +19,14 @@
 		 else{
 			 if(num>q->data)
 			 {
-				 search(q->right,num)
+				 search(q->right,num);
 			 }
+		 
 			 else{
 				 search(q->left,num);
 			 }
 		 }
 	 }
- }
 		 
  void traverse_inorder(node *q)
  {
@@ -47,24 +47,23 @@
 	 ptr->right=NULL;
 	 ptr->left=NULL;
 	 temp->right=ptr;
-	 return; 
+	 *r=ptr;
+ } 
 	 else{
 	 
 	if(num>temp->data)
-	{
-		if(temp->ptr->left==NULL);
 		{
-	ptr=(node*)malloc(sizeof(node));
-	ptr->data=num;		
-	 ptr->right=NULL;
-	 ptr->left=NULL;
-	 temp->right=ptr;
-	 return;
+			insert(&temp->right,num);
+		}
+		else 
+		{
+		insert(&temp->right,num);
 	}
+	}
+}
 		else {
 	temp=temp->right;
 	insert(&temp,num);
-}
 }
 else{
 	if(temp->left==NULL){
@@ -77,45 +76,108 @@ else{
 	}
 }
 }
-}
- void delete(node **q,int num)
+
+ 
+	 void search_node(node **x,node *root, node **parent, node **xsucc, int num,int *f)
  {
-	node *temp;
-	
-	 if(temp==NULL){
-		 printf("the given no is not found");
+	 node *temp;
+	 temp==root;
+	 if(temp==NULL)
+	 return;
+	 if(temp->data==num)
+	 {
+		 *f=1;
+		 *x=temp;
 		 return;
 	 }
-	 else{
-		 if(q->data==num){
-			if(temp->==NULL&&temp->right=NULL)
-			free(temp)
-			 
-		 return ;
-		 else{
-			 if(num>q->data)
-			 {
-				 delete(q->right,num)
-			 }
-			 else{
-				 delete(q->left,num);
-			 }
-		 }
-	 }
+	 while(temp!=NULL)
+	 {
+	 if(num>temp->data)
+	 temp=temp->right;
+	 else
+	 temp=temp->left;
  }
+	 
+ }
+ void delete(node **r, intx)
+ {
+	 node *temp, *parent, *xsucc;
+	 int f=0;
+	 parent=NULL;x=NULL xsucc=NULL;
+	 temp=*r;
+	 search_node(x,temp,&parent,&xsucc,&f);
+	 if(f==0){
+		 PRINTF("\n the element %d is not found",x);
+		 return;
+	 }
+	 if(x->left==NULL && x->right==NULL)
+	 {
+		 if(x->data>parent->data)
+		 parent->right=NULL;
+		 else
+		 parent->left=NULL;
+		 
+	 }
+	 else if(x->left!=NULL && x->right==NULL)
+	 {
+		 if(x->data>parent->data)
+		 parent->right=x->left;
+		 else
+		 parent->left=x->left;
+		 
+	 }
+	 else if(x->right!=NULL && x->left==NULL)
+	 {
+		 if(x->data>parent->data)
+		 parent->right=x->right;
+		 else
+		 parent->left=x->right;
+		 
+	}
+	else if(x->left!=NULL && x->right!=NULL)
+	{
+		parent=x;
+		xsucc=x->right;
+		while(xsucc->left!=NULL)
+		 {
+			 parent=xsucc;
+			 xsucc=xsucc->left;
+		 }
+		if(xsucc->data>x->data)
+		parent->right=NULL;
+		else
+		 parent->left=NULL;
+		 x->data=xsucc->data;
+		 x=xsucc;
+		 
+	}
+	free(x);
+}	 	  
+ 
 int main()
 {
-	node *root;
-	root=NULL;
-	insert(&root,5);
-	insert(&root,6);
-	insert(&root,7);
-	traverse_inorder(root);
-	if(search(root,8)==1)
-	{
-		printf("%dthe num %d is present in the tree",7);
-		else
-		printf("%dthe num %d is not found",7);
-		return 0;
-	}
+	node *root=NULL;
+    insert(&root,20);
+    insert(&root,15);
+    insert(&root,13);
+    insert(&root,17);
+    insert(&root,16);
+    insert(&root,19);
+    insert(&root,18);
+    insert(&root,25);
+    insert(&root,26);
+    
+    
+    traverse_inorder(root);
+    if(search_bst(root,30)==1)
+    {
+    printf("\n the number is found");
+    else
+    printf("\n the number is not found");
+    delete(&root,25);
+    tarverse_inorder(root);
+    return 0;
 }
+}
+    
+
